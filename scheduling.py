70ee38afb0bb7fbaf563b366b35fedb4eac248d6
Chris <chrisfjohnson3@gmail.com>
2019-09-30 18:46:18 -0700

    added to dos for layout.

--- scheduling.py
+++ scheduling.py
@@ -3,3 +3,51 @@ HW5 Scheduling
 9/30/19
 Chris Johnson
 """
+
+
+# TODO read text file containing processes for stack processes.txt [P3, 5, 8, 20}
+    #<process ID>, <arrival time>, <priority>, <CPU burst>
+
+
+# TODO print values read from file in format below, format below is order of arrival ->
+    #   P3, 5, 8, 20
+    #   P4, 9, 3, 50
+    #   P1, 9, 7, 35
+
+###########################################################################
+
+
+#TODO method to find lowest value in specific spot of array
+
+# TODO method to search processes for values that are greater or equal to current time
+
+# TODO method to print output of each scheduling process ->
+    # ---Scheduling results of <name of the scheduling algorithm>
+    # At time <…> ms, CPU starts running process <process ID>,
+    # at time <…> ms, CPU starts running process <process ID>,
+    # at time <…> ms, CPU starts running process <process ID>,
+    # ……
+    # ---End of the results of <name of the scheduling algorithm>
+
+
+
+############################################################################
+
+
+# TODO non preemptive short job first  (SJF)
+
+
+
+# TODO non preemptive priority scheduling
+
+
+
+# TODO Round Robin (RR) scheduling (10 milliseconds)
+
+
+
+################################################################################
+
+
+
+# TODO run on class server
