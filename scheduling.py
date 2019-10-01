08b49cfba5d542d5500f08eec2cf4e5223ababd2
chris <cjohn268@msudenver.edu>
2019-09-30 22:27:34 -0600

    made a nested dictionary for the processes from a file

--- scheduling.py
+++ scheduling.py
@@ -3,6 +3,7 @@ HW5 Scheduling
 9/30/19
 Chris Johnson
 """
+import re
 
 
 # TODO read text file containing processes for stack processes.txt [P3, 5, 8, 20}
@@ -14,10 +15,39 @@ Chris Johnson
     #   P4, 9, 3, 50
     #   P1, 9, 7, 35
 
+
+
 ###########################################################################
 
+def read_file(processes_file):
 
-#TODO method to find lowest value in specific spot of array
+    r_processes = dict(dict())
+
+    for line in processes_file:
+        stripped_line = line.rstrip('\n')
+        stripped_line = stripped_line.rstrip(' ')
+        line_read = stripped_line.split(",")
+        process_key = int(re.sub('\D', '', line_read[0]))
+
+        r_processes[process_key] = {
+                                    'process_id' : line_read[0],
+                                    'arrival_time' : line_read[1],
+                                    'priority' : line_read[2],
+                                    'cpu_burst' : line_read[3]
+                                    }
+
+    for keys in r_processes:
+        print(r_processes[keys]['process_id'] + ",",
+              r_processes[keys]['arrival_time'] + ",",
+              r_processes[keys]['priority'] + ",",
+              r_processes[keys]['cpu_burst'] )
+
+
+    return r_processes
+
+###############################################################################
+
+# TODO method to find lowest value in specific spot of array
 
 # TODO method to search processes for values that are greater or equal to current time
 
@@ -36,13 +66,23 @@ Chris Johnson
 
 # TODO non preemptive short job first  (SJF)
 
+def np_SJF(processes):
+
+    print("in sjf")
+
 
 
 # TODO non preemptive priority scheduling
 
+def np_Preemptive_scheduling(processes):
+    print("in np_pre_pri_sched")
+
 
 
 # TODO Round Robin (RR) scheduling (10 milliseconds)
+
+def round_robin(processes):
+    print("in round robin")
 
 
 
@@ -51,3 +91,13 @@ Chris Johnson
 
 
 # TODO run on class server
+
+if __name__ == '__main__':
+
+    processes_file = open('processes.txt', 'r')
+    processes = read_file(processes_file)
+    processes_file.close()
+    # processes is a dictionary inside dictionary
+    # 1st key is p ID number second id name like process id or cpu burst
+
+
